7256453d6d2558b6be446b161a8be4775296002d
timeloveboy <734991033@qq.com>
2019-06-12 06:05:18 +0800

    fx

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,5 @@
 docker build -t light4d/gojava -f gojava/Dockerfile --no-cache .
-docker push light4d/gojava
+
 docker build -t light4d/gonode -f gonode/Dockerfile --no-cache .
+docker push light4d/gojava
 docker push light4d/gonode
